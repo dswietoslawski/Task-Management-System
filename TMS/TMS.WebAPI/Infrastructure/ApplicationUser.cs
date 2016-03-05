b4199e6b15efc8a8739b24f01428414dd47a5e7c
Dawid Swietoslawski <dswietoslawski@gmail.com>
2016-03-05 15:51:36 +0100

    Set up WebAPI and user creation.

---
+++ TMS/TMS.WebAPI/Infrastructure/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TMS.WebAPI.Infrastructure {
+
+    public class ApplicationUser : IdentityUser {
+
+        [Required]
+        [MaxLength]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength]
+        public string LastName { get; set; }
+
+        [Required]
+        public byte Level { get; set; }
+
+        [Required]
+        public DateTime JoinDate { get; set; }
+    }
+}
---
+++ TMS/TMS.WebAPI/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Serialization;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web;
+using System.Web.Http;
+using TMS.WebAPI.Infrastructure;
+
+namespace TMS.WebAPI {
+    public class Startup {
+
+        public void Configuration(IAppBuilder app) {
+            HttpConfiguration httpConfig = new HttpConfiguration();
+            ConfigureOAuthTokenConfiguration(app);
+            ConfigureWebApi(httpConfig);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(httpConfig);
+        }
+
+        private void ConfigureWebApi(HttpConfiguration httpConfig) {
+            httpConfig.MapHttpAttributeRoutes();
+
+            var jsonFormatter = httpConfig.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+
+        private void ConfigureOAuthTokenConfiguration(IAppBuilder app) {
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+        }
+    }
+}
