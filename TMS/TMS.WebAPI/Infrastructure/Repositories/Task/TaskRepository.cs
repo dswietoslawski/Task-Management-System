fb1a903ae06b0b12b0b7a33bc156127ce2b00cfd
Dawid Swietoslawski <dswietoslawski@gmail.com>
2016-03-06 11:27:50 +0100

    Added empty task structure

---
+++ TMS/TMS.WebAPI/Infrastructure/Repositories/Task/TaskRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TMS.WebAPI.Infrastructure.Repositories.Task {
+    public class TaskRepository : ITaskRepository {
+
+        public void Add(TaskItem task) {
+            throw new NotImplementedException();
+        }
+
+        public TaskItem Find(string key) {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TaskItem> GetAll() {
+            throw new NotImplementedException();
+        }
+
+        public TaskItem Remove(string key) {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TaskItem item) {
+            throw new NotImplementedException();
+        }
+    }
+}
